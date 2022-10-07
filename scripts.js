f3a1c1e0130b2aaed9228299d71c06ef6191cf54
ow-tech <wagwanwekon@outlook.com>
2022-10-08 01:15:34 +0400

    home 3/4 done

---
+++ scripts.js
@@ -0,0 +1,28 @@
+
+// Get the container element
+let btnContainer = document.getElementById("nav-ul");
+
+// Get all buttons with class="btn" inside the container
+let btns = btnContainer.getElementsByClassName("nav-a");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+      let current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    });
+  }
+
+
+
+$(document).ready(function () {
+    $('.nav li a').click(function(e) {
+
+        $('.nav li a').removeClass('active');
+
+        let $parent = $(this).parent();
+        $parent.addClass('active');
+        e.preventDefault();
+    });
+});
