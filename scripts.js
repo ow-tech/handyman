6b6db2ff14f31417f8ec40047e1388cad679fdd2
ow-tech <wagwanwekon@outlook.com>
2022-10-09 15:52:58 +0400

    electrical ready

--- scripts.js
+++ scripts.js
@@ -1,5 +1,18 @@
+// load navbar
+// $(document).ready(function () {
+//   $(function () {
+//     // alert("am here");
+//     $("#headerSection").load("nav.html");
+//   });
+
+//   $.get("nav.html", function (data) {
+//     $("#headerSection").replaceWith(data);
+//   });
+// });
 
 // Get the container element
+
+// handle class active of nav
 let btnContainer = document.getElementById("nav-ul");
 
 // Get all buttons with class="btn" inside the container
@@ -7,22 +20,20 @@ let btns = btnContainer.getElementsByClassName("nav-a");
 
 // Loop through the buttons and add the active class to the current/clicked button
 for (let i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
-      let current = document.getElementsByClassName("active");
-      current[0].className = current[0].className.replace(" active", "");
-      this.className += " active";
-    });
-  }
-
-
+  btns[i].addEventListener("click", function () {
+    // alert('been clicked')
+    let current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
 
 $(document).ready(function () {
-    $('.nav li a').click(function(e) {
+  $(".nav li a").click(function (e) {
+    $(".nav li a").removeClass("active");
 
-        $('.nav li a').removeClass('active');
-
-        let $parent = $(this).parent();
-        $parent.addClass('active');
-        e.preventDefault();
-    });
+    let $parent = $(this).parent();
+    $parent.addClass("active");
+    e.preventDefault();
+  });
 });
